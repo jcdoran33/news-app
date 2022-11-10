da2ac38f556ae4f34e05d94b8932ef36cd3d2cd5
Jack Doran <jcdoran33@gmail.com>
2022-11-10 15:44:23 -0500

    laid out framework for API fetch

--- src/components/Container.js
+++ src/components/Container.js
@@ -11,13 +11,34 @@ import Feed from './Feed';
 import Footer from './Footer';
 import Header from './Header';
 
+
+//define constants - URL
+const apiKey = 123;
+let searchQuery;
+
 //house logic pertaining to creating an array of object of news stories once looped - API call
 const articlesArrObj = [
 
 ];
 
 //deinfe the function that will call API, and then store/append the article info to the articlesArrObj above
-
+function newsFetch () {
+    var apiURL = 'https://newsapi.org/v2/everything?q='+searchQuery+'&apiKey='+apiKey
+    fetch(apiURL)
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            //save required data from each loop
+            for (let i=0; i < 15; i++){
+                //save as a let variable the Title, Image, Description, and Hyperlink
+                let articleTitle = data.articles[i].title;
+                articlesArrObj.push(); //then push data to array as an Object
+            };
+            
+        })
+};
 
 export default function Container() {
     const [currentPage, setCurrentPage] = useState('Home'); //use this if creating multiple page site, to switch between pages

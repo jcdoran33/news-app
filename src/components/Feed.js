b1e2afcd4067d41fc950000adf59cd3a580ceee0
Jack Doran <jcdoran33@gmail.com>
2022-11-11 16:33:42 -0500

    began exploring search function

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,9 +3,33 @@ import React from 'react';
 import Article from './Article';
 
 function Feed(props) {
+    
+    const searchButton = document.querySelector("#search-button");
+    const searchBar = document.querySelector("#searchbar");
+
+    function runSearch() {
+        //save the text value as a const
+        const searchedText = searchBar.value;
+        console.log("Value of searchedText var: ", searchedText);
+        //clear the text field
+        searchBar.textContent = '';
+        //run the API fetch again using the search criteria as the search value
+
+    };
+    // searchButton.addEventListener("click", runSearch);
+
+    
+
+    
     return (
         <div className="container">
             <h5 className="text-center">News Feed Title</h5>
+
+            {/* area for search bar here */}
+
+            <label className="text-center" for="searchbar">Search for news:</label><br></br>
+            <input className="text-center" placeholder="Search for news..." type="text" id="searchbar" name="searchbar" required minLength="4" maxLength="35"></input>
+            <button id="search-button" className="text-center">Search</button>
 
             {props.articles.map((article, index) => (
                 

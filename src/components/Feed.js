d021663e10fdf80ce5419ac59644d0e207686a48
Jack Doran <jcdoran33@gmail.com>
2022-11-11 11:53:30 -0500

    news API fetch and display in React Feed component working successfully

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -6,16 +6,23 @@ function Feed(props) {
     return (
         <div className="container">
             <h5>Feed test text.</h5>
-            
-            {props.articles.map( (article, index) => (
+
+            {props.articles.map((article, index) => (
                 <li key={index}>
-                <Article articles={props.articles} key={index}/> {/* should articles equal to {article}?*/}
+                    <div className="card">
+                        <h5 className="article-title">{props.articles[index].title}</h5>
+                        <img className="article-img" src={props.articles[index].img} />
+                        <p className="article-description">{props.articles[index].desc}</p>
+                        <a className="article-link" href={props.articles[index].link}>Link to Full Article</a>
+                    </div>
+                    {console.log("TEST - index: ", index)}
+                    {/* <Article articles={props.articles} key={index}/>  */}
                 </li>
-                
+
             ))}
 
             {console.log("TEST - Feed props: ", props)}
-            {console.log("TEST - index from map: ", index)}
+
             {/* <Article /> */}
         </div>
     );
